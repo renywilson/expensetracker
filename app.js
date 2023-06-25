13cf3054e4fbb778bfefb990061f6cbed6818bfe
renywilson <116873667+renywilson@users.noreply.github.com>
2023-06-25 23:36:26 +0530

    Update app.js

--- app.js
+++ app.js
@@ -9,23 +9,18 @@ const Expense = require('./models/expenses');
 const Order = require('./models/orders');
 const Forgotpassword = require('./models/forgotpassword');
 const Download=require('./models/download');
-const helmet=require('helmet')
-const compression=require('compression')
-const morgan=require('morgan')
+
 const userRoutes = require('./routes/user')
 const expenseRoutes = require('./routes/expense')
 const purchaseRoutes = require('./routes/purchase')
 const premiumFeatureRoutes = require('./routes/premiumFeature')
 const resetPasswordRoutes = require('./routes/resetpassword')
-const accessLogStream=fs.createWriteStream(path.join(__dirname,'access.log'),{flags:'a'});
+
 const app = express();
 const dotenv = require('dotenv');
 require('dotenv').config();
 // get config vars
 dotenv.config();
-app.use(helmet());
-app.use(compression());
-app.use(morgan('combined',{stream:accessLogStream}));
 
 //console.log()
 app.use(cors());   
@@ -34,8 +29,7 @@ app.use(cors());
 
 
 app.use(express.json());  //this is for handling jsons
-const privatekey=fs.readFileSync('server.key')
-const certificate=fs.readFileSync('server.cert')
+
 
 app.use('/user', userRoutes)
 app.use('/expense', expenseRoutes)
