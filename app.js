188c4771e8b35561a8ccb55e0f02733019810199
renywilson <116873667+renywilson@users.noreply.github.com>
2023-06-28 17:42:15 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 const path = require('path');
-const fs=require('fs')
+
 const express = require('express');
 var cors = require('cors')
-const https=require('https')
+const app = express();
+
 const sequelize = require('./util/database');
 const User = require('./models/users');
 const Expense = require('./models/expenses');
@@ -16,28 +17,25 @@ const purchaseRoutes = require('./routes/purchase')
 const premiumFeatureRoutes = require('./routes/premiumFeature')
 const resetPasswordRoutes = require('./routes/resetpassword')
 
-const app = express();
-const dotenv = require('dotenv');
-require('dotenv').config();
-// get config vars
-dotenv.config();
 
 //console.log()
 app.use(cors());   
 
-// app.use(bodyParser.urlencoded());  ////this is for handling forms
-
-
+app.use(express.urlencoded());  ////this is for handling forms
 app.use(express.json());  //this is for handling jsons
+const dotenv = require('dotenv');
+require('dotenv').config()
+// get config vars
+dotenv.config();
 
-
+//console.log(process.env)
 app.use('/user', userRoutes)
 app.use('/expense', expenseRoutes)
 app.use('/purchase', purchaseRoutes)
 app.use('/premium', premiumFeatureRoutes)
 app.use('/password', resetPasswordRoutes);
 app.use((req,res)=>{
-   // console.log('urll',req.url);
+    console.log('urll',req.url);
  // res.sendFile(path.join(__dirname,'public/login.html')) 
   res.sendFile(path.join(__dirname,`public/${req.url}`))  
 })
@@ -50,12 +48,17 @@ User.hasMany(Forgotpassword);
 Forgotpassword.belongsTo(User);
 User.hasMany(Download)
 Download.belongsTo(User);
-//console.log(process.env.NODE_ENV);
+//console.log(require('dotenv').config())
+//console.log(process.env);
+
 sequelize.sync()
-    .then(() => {
+    .then(()=> {
    // https.createServer({key:privatekey,cert:certificate},app).listen(process.env.PORT||4500);
-   app.listen(process.env.PORT||4500);
+   app.listen(process.env.PORT||4500);;
+
+console.log('DB connection successful.');
+
     })
     .catch(err => {
-        console.log(err);
+        console.log(err,"DB connection not successful.");
     })
