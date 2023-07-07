421eba6b09db59f1ca2d476079b40a2cf3b097d0
renywilson <116873667+renywilson@users.noreply.github.com>
2023-07-07 14:16:44 +0530

    Add files via upload

--- middleware/auth.js
+++ middleware/auth.js
@@ -5,13 +5,13 @@ const authenticate = (req, res, next) => {
 
     try {
       const token = req.header('Authorization');
-      console.log(token);
+      //console.log(token);
       const decodedToken = jwt.verify(token, process.env.TOKEN_SECRET);
-      console.log('decoded token >>>>', decodedToken);
+      //console.log('decoded token >>>>', decodedToken);
       const userId = decodedToken.id;
-      console.log('userID >>>> ', userId);
+     // console.log('userID >>>> ', userId);
       User.findByPk(userId).then(user => {
-        console.log(JSON.stringify(user));
+       // console.log(JSON.stringify(user));
         req.user = user; ///very important
         next();
         })
--- util/database.js
+++ util/database.js
@@ -1,12 +1,17 @@
-const dotenv = require('dotenv');  
-dotenv.config();
-var mysql = require('mysql');
+
 const Sequelize = require('sequelize');
+const dotenv = require('dotenv');
+
+// get config vars
+dotenv.config();
+
+
+console.log(process.env)
 const sequelize = new Sequelize(process.env.DATABASE, process.env.USER_NAME, process.env.PASSWORD,{
     dialect: 'mysql',
     host: process.env.DB_HOST
      
 });
-console.log(process.env)
+
 console.log('Database connected');
 module.exports = sequelize;
